459ce5501394b8d8ca9d010303d114208526681c
David Lambauer <david@run-as-root.sh>
2026-04-14 13:00:05 +0200

    feat: add extraction rule edit form with CRUD controllers (#7)

---
+++ Controller/Adminhtml/ExtractionRule/NewAction.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\CatalogDataAI\Controller\Adminhtml\ExtractionRule;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+class NewAction extends Action implements HttpGetActionInterface
+{
+    public const ADMIN_RESOURCE = 'MageOS_CatalogDataAI::extraction_rules';
+
+    public function execute()
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_FORWARD)->forward('edit');
+    }
+}
