4fd0cd323d61a369db3ed8e69bd6ec06e771a112
David Lambauer <david@run-as-root.sh>
2026-04-14 12:20:34 +0200

    feat: add approved and rejected statuses to EnrichmentLog (#28)

--- Model/EnrichmentLog.php
+++ Model/EnrichmentLog.php
@@ -11,6 +11,8 @@ class EnrichmentLog extends AbstractModel
     public const STATUS_GENERATED = 'generated';
     public const STATUS_PENDING_REVIEW = 'pending_review';
     public const STATUS_MODIFIED = 'modified';
+    public const STATUS_APPROVED = 'approved';
+    public const STATUS_REJECTED = 'rejected';
 
     protected function _construct(): void
     {
